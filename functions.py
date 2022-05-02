65628fc09c8239eb6256dc29a3921c0a6c90c9df
josephmlev <joseph@campus-014-109.ucdavis.edu>
2022-05-02 11:44:58 -0700

    brings functions.py and main up to date with end of meeting 5.2.22. dataObject class, model and loglike functions all should be working smoothly and well commented

--- functions.py
+++ functions.py
@@ -7,16 +7,24 @@ Created on Sun May  1 14:53:35 2022
 """
 import pandas as pd
 import numpy as np
+from astropy.cosmology import LambdaCDM
+import astropy.units as u
 
 class dataObject:
     '''
     Inputs:
-        STR: path
+    --------------------   
+    covType: string
+        Type of covariance matrix to be computed. Can be 'sys' to only include
+        systamatics or 'tot' to include systamatic + statistical matricies
+        
+    path = ('./data/'): string
         Relitive path to directory which has two data files. 
         Defaults to ./data/
         Can be set using the changePath method
         
     Attributes:
+    --------------------
         Pandas DF: dataDf
         data frame with columns for zcmb, mb, and dmb. 40 rows
         
@@ -35,11 +43,15 @@ class dataObject:
         covStat: np 2D array
         40x40 diagonal statistical covariance matrix
         
-        covTotal: np 2D array
-        40x40 total covariance matrix. Sum of covStat + covSys
+        cov: np 2D array
+        40x40 total covariance matrix. Either total (Sum of covStat + covSys)
+        or covStat depending on covType
+        
+        covInv: np 2D array
+        40x40 inverse of covariance matrix
         '''
         
-    def __init__(self, path = ('./data/')):        
+    def __init__(self, covType, path = ('./data/')):        
         '''
         Constructor method for dataObject class
         
@@ -49,46 +61,47 @@ class dataObject:
         Systamatic covariance matrix is given in sys_DS17f.txt as a 1600 
         element list and is reshaped into a 40x40 numpy array
         '''
+        
+
+        if covType != 'sys' and covType != 'tot':
+            raise TypeError('covType must = "sys" or "tot"')
+        
+        self.covType = covType
         self.path = path
         
         #import raw data and drop all unused params
-        lcParam = pd.read_csv(self.path+'lcparam_DS17f.txt', delimiter= ' ')
-        self.dataDf = lcParam.drop(['#name',
-                               'zhel',
-                               'dz',
-                               'x1',
-                               'dx1',
-                               'color',
-                               'dcolor',
-                               '3rdvar',
-                               'd3rdvar',
-                               'cov_m_s',
-                               'cov_m_c',
-                               'cov_s_c',
-                               'set',
-                               'ra',
-                               'dec',
-                               'biascor',
-                               'Unnamed: 19'], 
-                              axis = 1)
+        self.dataDf = pd.read_csv(self.path+'lcparam_DS17f.txt',
+                              delimiter= ' ')
         
-        self.zcmb = np.asarray(self.dataDf['zcmb'])
-        self.mb = np.asarray(self.dataDf['mb'])
-        self.dmb = np.asarray(self.dataDf['dmb'])        
+        #set useful params to attributes as np arrays (type float32)
+        self.zcmb = np.asarray(self.dataDf['zcmb'],
+                               dtype = np.float32)
+        self.mb = np.asarray(self.dataDf['mb'],
+                             dtype = np.float32)
+        self.dmb = np.asarray(self.dataDf['dmb'],
+                              dtype = np.float32)        
 
         #import raw systamatic cov list. Drop first element 
         #(first element is 40, refering to size of matrix)
-        sysRaw = np.loadtxt(self.path+'sys_DS17f.txt', skiprows = 1)
+        sysRaw = np.loadtxt(self.path+'sys_DS17f.txt',
+                            skiprows = 1,
+                            dtype = np.float32 )
         #reshape 1600x1 list into 40x40 array
         self.covSys = sysRaw.reshape(40,40)
         
         #pull statastical error from dataDf and pack into 
         #diagonal elements of 40x40 matrix
-        self.covStat = np.diag(self.dataDf['dmb'])
+        self.covStat = np.diag(self.dmb)**2
         
         #total cov is sum of statistical and systamatic 
-        self.covTotal = self.covStat + self.covSys
+        self.cov = self.covStat
+        if (self.covType == 'sys'):
+            self.cov = self.covStat + self.covSys
+            
+        self.covInv = np.linalg.inv(self.cov)
     
+    
+
     def changePath(self, path):
         '''
         changePath method used to change path (duh)
@@ -98,4 +111,68 @@ class dataObject:
         '''
         self.path = path
         print('path is changed to', path)
+        
+    def changeCov(self, covType):
+        if covType != 'sys' and covType != 'tot':
+            raise TypeError('covType must = "sys" or "tot"')
+        else:
+            self.covType = covType
     
+def loglike(params, data):
+    '''
+    Computes log likliehood from parameter array
+    
+    Paramters:
+    --------------------
+    params: 1D array
+        cosmological parameters and nuiansence parameters
+        params = [H0, Om_matter, Om_lambda, M]
+        The order of parameters is [Hubble constant, matter density, dark 
+        energy density, absolute magnitude]
+        
+    data: dataObject
+        obbject containing data as attributes 
+        
+    Return:
+    --------------------
+    chi2: float32 
+    sum over trials of -((data - model)_i * cov_ij * (data - model)_j)/2. 
+    Computed as matrix multplication 
+        
+    
+    '''
+    m = model(data.zcmb, params)
+    deltaM =  data.mb - m
+    chi2 = deltaM @ data.covInv @ deltaM
+    return(-chi2/2)
+
+
+
+def model(z, params):
+    '''
+    calculate the apparent magnitude from a model
+
+    Paramters:
+    --------------------
+    z: float or 1D array
+        The redshift(s) of the observations
+    params: 1D array
+        cosmological parameters and nuiansence parameters
+        params = [H0, Om_matter, Om_lambda, M]
+        The order of parameters is [Hubble constant, matter density, dark energy density, absolute magnitude]
+    
+    Return:
+    --------------------
+    mb_th: float or 1D array
+        the theoretical apparent magnitude of the SNe targets given the redshifts under a certain parameters
+    '''
+
+    # pass parameters to the variables
+    h0, omm, oml, M = params
+
+    # initiate the cosmology
+    cosmo = LambdaCDM(H0=h0*u.km/u.s/u.Mpc, Om0 = omm, Ode0=oml)
+
+    dmoduli = np.array(cosmo.distmod(z))
+
+    return dmoduli + M
