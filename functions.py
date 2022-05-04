4e5e00383161cdebebe56492f218d4582310c4b2
shuimufge <fge@ucdavis.edu>
2022-05-04 11:02:00 -0700

    add functions and examples for plotting

--- functions.py
+++ functions.py
@@ -9,6 +9,8 @@ import pandas as pd
 import numpy as np
 from astropy.cosmology import LambdaCDM
 import astropy.units as u
+import matplotlib.pyplot as plt
+from getdist import MCSamples, plots
 
 class dataObject:
     '''
@@ -176,3 +178,86 @@ def model(z, params):
     dmoduli = np.array(cosmo.distmod(z))
 
     return dmoduli + M
+
+class make_plot:
+    '''
+    Inputs:
+    -----------
+    chains: a list of 2D array
+        A list of the chains from MCMC runner
+        For a single chain, each column is for one variable, each row is a record for a single step
+
+    legend: a list of strings
+        A list of the legend for each MCMC chains
+
+    label: a list of strings
+        A list of the label for each variable in the MCMC chains
+        We assume that each chain has the same variables
+    
+    name: list of strings
+        name of each variable in a chain
+        We assume that each chain has the same variables
+    
+    burn_in: float
+        fraction of the MCMC chains to remove due to burn_in
+    
+    Attributes:
+    -----------
+    samples: list of MCSample object of GetDist
+        list of MCSample object of GetDist from the input chains
+    
+    name: list of strings
+        name of each variable in the chains
+    
+    label: list of LaTex strings
+        LaTex of each variable
+    '''
+
+    def __init__(self, chains, legend=None, label=None, name=None, burn_in=0.2):
+        '''
+        Constructor method for make_plot class
+        '''
+
+        if legend is None:
+            self.legend = ['sample%i'%(i+1) for i in range(len(chains))]
+        else:
+            self.legend = legend
+
+        if label is None:
+            self.label = ['x_%i'%(i+1) for i in range(np.shape(chains[0])[1])]
+        else:
+            self.label = label
+        
+        if name is None:
+            self.name = ['x%i'%(i+1) for i in range(np.shape(chains[0])[1])]
+        else:
+            self.name = name
+        
+        self.samples = []
+        for i in range(len(chains)):
+            self.samples.append(MCSamples(samples = chains[i], names=self.name, labels=self.label, ignore_rows = burn_in))
+        
+    def plot_2d(self, var_index=[0,1]):
+        '''
+        Input:
+        ---------------
+        var_index: 1D list of int
+            the index of the variables to plot
+        '''
+        i, j = var_index
+        g = plots.get_single_plotter(width_inch = 10)
+        g.plot_2d(self.samples, [self.name[i], self.name[j]])
+
+        g.finish_plot(legend_labels=self.legend)
+    
+    def plot_1d(self, var_index=0):
+        '''
+        Input:
+        ---------------
+        var_index: int
+            the index of the variable to plot
+        '''
+        g = plots.get_single_plotter(width_inch = 10)
+        g.plot_1d(self.samples, self.name[var_index])
+
+        g.finish_plot(legend_labels=self.legend)
